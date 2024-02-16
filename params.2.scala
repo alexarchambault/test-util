f9b6a539e9ca8461703dfee811d50211d8459dc0
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2024-02-16 19:26:17 +0100

    Cross-compile things (#1)

---
+++ params.2.scala
@@ -0,0 +1,3 @@
+//> using target.scala "2"
+//> using dep "io.get-coursier::coursier-cache:2.1.9"
+//> using option "-Xsource:3", "-Ywarn-unused"
---
+++ params.3.scala
@@ -0,0 +1,2 @@
+//> using target.scala "3"
+//> using dep "io.get-coursier:coursier-cache_2.13:2.1.9"
